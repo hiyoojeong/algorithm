a4a7749975fc05065f3c73a9c217bd82ff0edbbd
조유정 <99192837+hiyoojeong@users.noreply.github.com>
2024-05-14 20:23:42 +0900

    [Gold V] Title: 전깃줄, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Gold/2565. 전깃줄/전깃줄.java
+++ 백준/Gold/2565. 전깃줄/전깃줄.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
@@ -19,6 +20,8 @@ public class Main {
 		}
 
 	}
+
+	static List<Integer> lis;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -44,24 +47,45 @@ public class Main {
 
 		});
 
-		// 오른쪽 전봇대를 기준으로 '가장 긴 증가하는 수열'의 길이를 찾는다.
-		int[] length = new int[n];
-		int maxLength = 1;
-		for (int k = 0; k < n; k++) {
-			length[k] = 1;
-			for (int i = 0; i < k; i++) {
-				if (lines.get(i).right < lines.get(k).right) {
-					length[k] = Math.max(length[k], length[i] + 1);
-				}
+		// 오른쪽 전봇대를 기준으로 '가장 긴 증가하는 수열'의 길이를 찾는다. - 이진탐색으로 LIS를 만드는 방법 활용
+		lis = new ArrayList<>();
+		lis.add(lines.get(0).right);
+
+		for (int i = 1; i < n; i++) {
+			int value = lines.get(i).right;
+			int index = binarySearch(value);
+
+			if (lis.get(index) < value) {
+				lis.add(value);
+			} else {
+				lis.set(index, value);
 			}
-			maxLength = Math.max(maxLength, length[k]);
 		}
+		int maxLength = lis.size();
 
 		// 없애야 하는 전깃줄의 개수 = 전체 전깃줄의 개수 - 있어야 하는 전깃줄의 개수(가장 긴 증가하는 수열의 개수)
 		int answer = n - maxLength;
 
 		System.out.println(answer);
 
+	}
+
+	public static int binarySearch(int value) {
+		int left = 0;
+		int right = lis.size() - 1;
+
+		// Upperbound(상계): 찾으려는 값보다 큰 값(초과)이 나타나는 시점을 찾는다.
+		while (left < right) {
+			int mid = (left + right) / 2;
+
+			if (lis.get(mid) < value) {
+				left = mid + 1;
+			} else {
+				right = mid;
+			}
+		}
+
+		return right;
 	}
 
 }
