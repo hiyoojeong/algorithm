f2601bb52b592cea9af99e00878a0617c2d98e0d
조유정 <99192837+hiyoojeong@users.noreply.github.com>
2024-05-20 15:27:31 +0900

    [Gold V] Title: 평범한 배낭, Time: 152 ms, Memory: 14600 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -16,7 +16,7 @@ public class Main {
 
 		int[] weight = new int[N + 1];
 		int[] value = new int[N + 1];
-		int[][] dp = new int[N + 1][K + 1]; // 최대 무게 K인 가방에 담을 수 있는 N번째 물건까지의 최대 가치
+		int[] dp = new int[K + 1]; // 최대 무게 K인 가방에 담을 수 있는 최대 가치
 		for (int i = 1; i <= N; i++) {
 			st = new StringTokenizer(br.readLine());
 			int w = Integer.parseInt(st.nextToken());
@@ -27,21 +27,16 @@ public class Main {
 		}
 
 		for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= K; j++) {
-				// 담을 수 있는 경우
-				if (j >= weight[i]) {
-					// 담지 않는 경우: dp[i - 1][j]
-					// 담는 경우: value[i] + dp[i - 1][j - weight[i]]
-					dp[i][j] = Math.max(dp[i - 1][j], value[i] + dp[i - 1][j - weight[i]]);
-				}
-				// 담을 수 없는 경우
-				else {
-					dp[i][j] = dp[i - 1][j];
-				}
+
+			// 담을 물건 수를 늘려가면서,
+			// 최대 무게(K)부터 담을 수 있는 무게까지만 탐색한다.
+			for (int j = K; j - weight[i] >= 0; j--) {
+				dp[j] = Math.max(dp[j], value[i] + dp[j - weight[i]]);
 			}
+
 		}
 
-		System.out.println(dp[N][K]);
+		System.out.println(dp[K]);
 
 	}
 
