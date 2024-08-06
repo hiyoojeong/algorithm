b4d873651e6c6c652ee87edcfba48cf777886874
조유정 <99192837+hiyoojeong@users.noreply.github.com>
2024-08-06 23:16:30 +0900

    [Gold IV] Title: 회전 초밥, Time: 936 ms, Memory: 326060 KB -BaekjoonHub

--- 백준/Gold/15961. 회전 초밥/회전 초밥.java
+++ 백준/Gold/15961. 회전 초밥/회전 초밥.java
@@ -10,7 +10,7 @@ public class Main {
 
     static int N, d, k, c;
     static int[] plates;
-    static Map<Integer, Integer> map;
+    static Map<Integer, Integer> info;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -20,7 +20,7 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken()); // 회전 초밥 벨트에 놓인 접시의 수
         d = Integer.parseInt(st.nextToken()); // 초밥의 가짓수
-        k = Integer.parseInt(st.nextToken()); // 연속해서 먹는 접시의 수
+        k = Integer.parseInt(st.nextToken()); // 연속해서 먹는 접의 수
         c = Integer.parseInt(st.nextToken()); // 쿠폰 번호
 
         plates = new int[N];
@@ -28,39 +28,58 @@ public class Main {
             plates[i] = Integer.parseInt(br.readLine());
         }
 
-        map = new HashMap<>();
+        info = new HashMap<>();
 
-        // K-1개를 미리 먹어둔다.
-        for (int i = 0; i < k - 1; i++) {
-            map.put(plates[i], map.getOrDefault(plates[i], 0) + 1);
-        }
+        // 투 포인터
+        int max = 1, cnt = 1;
+        int s = 0, e = 1;
 
-        // 먹기 -> 업데이트 -> 뱉기를 반복한다.
-        int max = 0;
-        int e = k - 1;
-        for (int s = 0; s < N; s++) {
-            int remove = plates[s];
-            int eat = plates[e];
+        info.put(plates[s], 1);
 
-            // 먹는다.
-            map.put(eat, map.getOrDefault(eat, 0) + 1);
+        boolean isStart = true;
+        while (s != 0 || isStart) {
+            // 초밥을 연속해서 더 먹을 수 있는 경우
+            if (cnt < k) {
+                // 현재까지 먹은 접시 수를 늘린다.
+                cnt++;
 
-            // 업데이트한다.
-            if (map.containsKey(c)) {
-                max = Math.max(map.size(), max);
-            } else {
-                max = Math.max(map.size() + 1, max);
+                // 현재까지 먹은 초밥 종류를 업데이트한다.
+                int next = plates[e];
+                info.put(next, info.getOrDefault(next, 0) + 1);
+
+                // 끝 인덱스를 다음 접시로 이동한다.
+                e = (e + 1) % N;
             }
+            // 초밥을 연속해서 최대로 다 먹은 경우
+            else {
+                // 초밥 가짓수 업데이트
+                // 추가 초밥이 이미 먹은 것이므로, 먹은 초밥 가짓수는 동일하다.
+                if (info.containsKey(c)) {
+                    max = Math.max(info.size(), max);
+                }
+                // 추가 초밥이 새로운 것이므로, 먹은 초밥 가짓수는 1개 더 많아진다.
+                else {
+                    max = Math.max(info.size() + 1, max);
+                }
 
-            // 뱉는다.
-            map.put(remove, map.get(remove) - 1);
-            if (map.get(remove) == 0) {
-                map.remove(remove);
+                // 먹었던 초밥 뱉기
+                // 현재까지 먹은 접시 수를 줄인다.
+                cnt--;
+
+                // 현재까지 먹은 초밥 종류를 업데이트한다.
+                int front = plates[s];
+                info.put(front, info.get(front) - 1);
+                if(info.get(front) == 0) {
+                    info.remove(front);
+                }
+
+                // 시작 인덱스를 다음 접시로 이동한다.
+                s = (s + 1) % N;
+                isStart = false;
             }
-
-            e = (e + 1) % N;
         }
 
         System.out.println(max);
+
     }
 }
