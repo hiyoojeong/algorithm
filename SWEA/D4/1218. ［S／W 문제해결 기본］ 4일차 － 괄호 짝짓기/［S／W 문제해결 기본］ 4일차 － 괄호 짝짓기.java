af2836335573fe563282120502dbe3f739be544f
조유정 <99192837+hiyoojeong@users.noreply.github.com>
2024-08-14 17:16:38 +0900

    [D4] Title: [S/W 문제해결 기본] 4일차 - 괄호 짝짓기, Time: 139 ms, Memory: 20,432 KB -BaekjoonHub

--- SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
+++ SWEA/D4/1218. ［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기/［S／W 문제해결 기본］ 4일차 － 괄호 짝짓기.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 // 괄호 짝짓기
 public class Solution {
@@ -9,12 +10,12 @@ public class Solution {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		StringBuffer answer = new StringBuffer();
-		
+
 		int T = 10;
 		for (int test_case = 1; test_case <= T; test_case++) {
 			int N = sc.nextInt();
 			String str = sc.next();
-			
+
 			int res = isPair(N, str);
 			answer.append(String.format("#%d %d\n", test_case, res));
 		}
@@ -25,62 +26,29 @@ public class Solution {
 	}
 
 	public static int isPair(int N, String str) {
-		int cnt1 = 0; // ()
-		int cnt2 = 0; // <>
-		int cnt3 = 0; // {}
-		int cnt4 = 0; // []
+		Stack<Character> stack = new Stack<>();
 
 		for (int i = 0; i < N; i++) {
 			char ch = str.charAt(i);
 
-			// ()
-			if (ch == '(') {
-				cnt1++;
-			} else if (ch == ')') {
-				cnt1--;
-				// 열린 괄호가 없는데 닫힌 괄호를 사용함
-				if (cnt1 < 0) {
-					return FALSE;
-				}
+			// 열린 괄호는 stack에 저장한다.
+			if (ch == '(' || ch == '[' || ch == '<' || ch == '{') {
+				stack.push(ch);
 			}
+			// 닫힌 괄호는 stack에서 가져온 괄호롸 짝이 맞아야 한다.
+			else {
+				// stack에서 가져온 괄호
+				char pair_ch = stack.pop();
 
-			// <>
-			if (ch == '<') {
-				cnt2++;
-			} else if (ch == '>') {
-				cnt2--;
-				// 열린 괄호가 없는데 닫힌 괄호를 사용함
-				if (cnt2 < 0) {
-					return FALSE;
+				// 짝이 맞는 경우
+				if ((pair_ch == '(' && ch == ')') || (pair_ch == '[' && ch == ']') || (pair_ch == '<' && ch == '>')
+						|| (pair_ch == '{' && ch == '}')) {
+					continue;
 				}
-			}
 
-			// {}
-			if (ch == '{') {
-				cnt3++;
-			} else if (ch == '}') {
-				cnt3--;
-				// 열린 괄호가 없는데 닫힌 괄호를 사용함
-				if (cnt3 < 0) {
-					return FALSE;
-				}
+				// 짝이 안 맞는 경우
+				return FALSE;
 			}
-
-			// []
-			if (ch == '[') {
-				cnt4++;
-			} else if (ch == ']') {
-				cnt4--;
-				// 열린 괄호가 없는데 닫힌 괄호를 사용함
-				if (cnt4 < 0) {
-					return FALSE;
-				}
-			}
-		}
-
-		// 열린 괄호가 있는데 닫힌 괄호를 사용하지 않음
-		if (cnt1 > 0 || cnt2 > 0 || cnt3 > 0 || cnt4 > 0) {
-			return FALSE;
 		}
 
 		return TRUE;
