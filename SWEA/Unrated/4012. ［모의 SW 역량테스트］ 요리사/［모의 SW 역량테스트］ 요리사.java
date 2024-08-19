5a97d889f90af8169d5ceefb44098035a409e9f3
조유정 <99192837+hiyoojeong@users.noreply.github.com>
2024-08-19 16:55:49 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 353 ms, Memory: 53,368 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -1,20 +1,7 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
-// 요리사
+// 4012 [모의 SW 역량테스트] 요리사
 public class Solution {
-
-	static int N; // 식재료의 개수
-	static int[][] map; // 식재료들의 시너지 값
-
-	static boolean[] isDivided; // true : A 음식, false : B 음식
-	static final boolean A = true;
-	static final boolean B = false;
-
-	static int[] selected; // 시너지를 게산하기 위해 선택한 두 식재료 정보
-	static int synergyA, synergyB; // A 음식의 시너지, B 음식의 시너지
-
-	static int minDist; // 두 음식 간의 최소 맛 차이
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -23,9 +10,9 @@ public class Solution {
 		StringBuffer answer = new StringBuffer();
 		for (int test_case = 1; test_case <= T; test_case++) {
 			// 입력
-			N = sc.nextInt();
-			map = new int[N][N];
-			minDist = Integer.MAX_VALUE;
+			int N = sc.nextInt();
+			int[][] map = new int[N][N];
+			int minDist = Integer.MAX_VALUE;
 
 			for (int i = 0; i < N; i++) {
 				for (int j = 0; j < N; j++) {
@@ -33,11 +20,58 @@ public class Solution {
 				}
 			}
 
-			isDivided = new boolean[N];
-			selected = new int[2];
+			// N = 4라면, 2^4가지 선택할 수 있는 경우의 수가 존재한다. 이는 '1 << N'와 같다.
+			// 00000
+			// 00001
+			// 00010
+			// 00011
+			// 00100
+			// 00101
+			// ...
+			// 10000
 
-			// 식재료를 A 음식에 넣을지, B 음식에 넣을지 선택한다.
-			divide(0, 0);
+			// 그러나, N / 2만큼의 식재료로 음식을 만든다.
+			// 그렇기 때문에 2^4 / 2가지 경우의 수만 확인해보면 된다. 이는 '(1 << N) / 2'와 같다.
+			// 000
+			// 001
+			// 010
+			// ...
+			// 100
+			for (int i = 0; i < (1 << N) / 2; i++) {
+				int cnt = 0;
+				boolean[] food = new boolean[N]; // true = A 음식 식재료, false = B 음식 식재료
+
+				for (int j = 0; j < N; j++) {
+					if ((i & (1 << j)) != 0) { // bit가 1인 경우, A 음식의 식재료로 선택된다.
+						food[j] = true;
+						cnt++;
+					}
+				}
+
+				if (cnt == N / 2) { // 식재료가 반반씩 나눠졌다!
+					int sumA = 0;
+					int sumB = 0;
+
+					for (int k = 0; k < N; k++) {
+						for (int l = 0; l < N; l++) {
+							if (k == l) { // 동일한 식재료를 사용할 수 없다.
+								continue;
+							}
+							if (food[k] != food[l]) { // 두 식재료가 같은 음식에 사용되어야 한다.
+								continue;
+							}
+							if (food[k]) { // 두 식재료가 모두 A 음식에 사용되는 경우
+								sumA += map[k][l];
+
+							} else { // 두 식재료가 모두 B 음식에 사용되는 경우
+								sumB += map[k][l];
+							}
+						}
+					}
+
+					minDist = Math.min(minDist, Math.abs(sumA - sumB));
+				}
+			}
 
 			answer.append(String.format("#%d %d\n", test_case, minDist));
 		}
@@ -45,62 +79,6 @@ public class Solution {
 		System.out.println(answer);
 
 		sc.close();
-	}
-
-	public static void divide(int cnt, int index) {
-		if (index == N) {
-			// 식재료들이 각각 N / 2개씩 나누어 두 개의 요리를 만들어냈다.
-			if (cnt == N / 2) {
-				// A 음식을 만들 때 시너지 구하기
-				synergyA = 0;
-				getSynergyA(0, 0);
-
-				// B 음식을 만들 때 시너지 구하기
-				synergyB = 0;
-				getSynergyB(0, 0);
-
-				// 두 음식 간의 맛의 차이가 최소가 되는 경우라면, 업데이트한다.
-				minDist = Math.min(Math.abs(synergyA - synergyB), minDist);
-			}
-			return;
-		}
-
-		// 현재 식재료를 A 음식으로 만드는 경우
-		isDivided[index] = A;
-		divide(cnt + 1, index + 1);
-		// 현재 식재료를 B 음식으로 만드는 경우
-		isDivided[index] = B;
-		divide(cnt, index + 1);
-	}
-
-	public static void getSynergyA(int cnt, int start) {
-		if (cnt == 2) {
-			synergyA += map[selected[0]][selected[1]];
-			synergyA += map[selected[1]][selected[0]];
-			return;
-		}
-
-		for (int i = start; i < N; i++) {
-			if (isDivided[i] == A) {
-				selected[cnt] = i;
-				getSynergyA(cnt + 1, i + 1);
-			}
-		}
-	}
-
-	public static void getSynergyB(int cnt, int start) {
-		if (cnt == 2) {
-			synergyB += map[selected[0]][selected[1]];
-			synergyB += map[selected[1]][selected[0]];
-			return;
-		}
-
-		for (int i = start; i < N; i++) {
-			if (isDivided[i] == B) {
-				selected[cnt] = i;
-				getSynergyB(cnt + 1, i + 1);
-			}
-		}
 	}
 
 }
