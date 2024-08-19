a882d5d83f3851ae38ebb9eeb73bc118bbd07252
조유정 <99192837+hiyoojeong@users.noreply.github.com>
2024-08-20 00:17:35 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 572 ms, Memory: 69524 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -2,111 +2,122 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
-// 특정한 최단경로
+// 특정한 최단 경로
 public class Main {
 
-	static class Node {
-		int idx, cost;
+    static class Node {
 
-		public Node(int idx, int cost) {
-			this.idx = idx;
-			this.cost = cost;
-		}
+        int idx, cost;
 
-	}
+        public Node(int idx, int cost) {
+            this.idx = idx;
+            this.cost = cost;
+        }
+    }
 
-	static List<Node>[] gragh;
-	static int[] dist;
+    static List<Node>[] graph; // 인접한 리스트 정보 저장
+    static int[] dist; // 출발 지점에서 다른 지점까지의 최단 거리를 저장
 
-	static int BIG_VALUE = Integer.MAX_VALUE;
+    public static void main(String[] args) throws NumberFormatException, IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()); // 정점의 수
+        int E = Integer.parseInt(st.nextToken()); // 간선의 수
 
-		st = new StringTokenizer(br.readLine());
-		int V = Integer.parseInt(st.nextToken());
-		int E = Integer.parseInt(st.nextToken());
+        // 인접한 정점 저장 배열 초기화
+        graph = new ArrayList[N + 1];
+        dist = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            graph[i] = new ArrayList<>();
+        }
 
-		// 인접 그래프와 최단거리 배열을 초기화한다.
-		gragh = new ArrayList[V + 1];
-		dist = new int[V + 1];
-		for (int i = 1; i < V + 1; i++) {
-			gragh[i] = new ArrayList<>();
-		}
+        // 인접한 정점 저장
+        for (int i = 0; i < E; i++) {
+            st = new StringTokenizer(br.readLine());
+            int n1 = Integer.parseInt(st.nextToken()); // 정점
+            int n2 = Integer.parseInt(st.nextToken()); // 정점
+            int cost = Integer.parseInt(st.nextToken()); // 정점 간 거리
 
-		// 인접 그래프의 간선 정보를 저장한다.
-		for (int i = 0; i < E; i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			gragh[a].add(new Node(b, c));
-			gragh[b].add(new Node(a, c));
-		}
+            graph[n1].add(new Node(n2, cost));
+            graph[n2].add(new Node(n1, cost));
+        }
 
-		// 반드시 거쳐야 하는 두 개의 정점을 저장한다.
-		st = new StringTokenizer(br.readLine());
-		int v1 = Integer.parseInt(st.nextToken());
-		int v2 = Integer.parseInt(st.nextToken());
+        // 방문해야 하는 정점 저장
+        st = new StringTokenizer(br.readLine());
+        int v1 = Integer.parseInt(st.nextToken()); // 방문해야 하는 정점
+        int v2 = Integer.parseInt(st.nextToken()); // 방문해야 하는 정점
 
-		// 1) v1을 시작점으로 하는 최단경로 구하기
-		for (int i = 1; i < V + 1; i++) {
-			dist[i] = BIG_VALUE;
-		}
-		dijkstra(v1);
-		int startToV1 = dist[1]; // 1 -> v1
-		int v1ToV2 = dist[v2]; // v1 -> v2
-		int v1ToN = dist[V]; // v1 -> n
+        // 최단거리 구하기 - 1번 정점에서 출발
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[1] = 0;
+        dijkstra(1);
 
-		// 2) v2를 시작점으로 하는 최단경로 구하기
-		for (int i = 1; i < V + 1; i++) {
-			dist[i] = BIG_VALUE;
-		}
-		dijkstra(v2);
-		int startToV2 = dist[1]; // 1 -> v2
-		int v2ToV1 = dist[v1]; // v2 -> v1
-		int v2ToN = dist[V]; // v2 -> n
+        int one_to_v1 = dist[v1];
+        int one_to_v2 = dist[v2];
 
-		int v1Cost = BIG_VALUE, v2Cost = BIG_VALUE;
-		if (startToV1 != BIG_VALUE && v1ToV2 != BIG_VALUE && v2ToN != BIG_VALUE) {
-			// 1 -> v1 -> v2 -> n
-			v1Cost = startToV1 + v1ToV2 + v2ToN;
-		}
-		if (startToV2 != BIG_VALUE && v2ToV1 != BIG_VALUE && v1ToN != BIG_VALUE) {
-			// 1 -> v2 -> v1 -> n
-			v2Cost = startToV2 + v2ToV1 + v1ToN;
-		}
+        // 최단거리 구하기 - N번 정점에서 출발
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[N] = 0;
+        dijkstra(N);
 
-		int answer = Math.min(v1Cost, v2Cost);
-		System.out.println(answer == BIG_VALUE ? "-1" : answer);
+        int v1_to_N = dist[v1];
+        int v2_to_N = dist[v2];
 
-	}
+        // 최단거리 구하기 - v1번 정점에서 출발
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[v1] = 0;
+        dijkstra(v1);
 
-	public static void dijkstra(int startNode) {
-		dist[startNode] = 0;
+        int v1_to_v2 = dist[v2];
+        int v2_to_v1 = v1_to_v2;
 
-		Queue<Node> queue = new PriorityQueue<Node>((o1, o2) -> o1.cost - o2.cost);
-		queue.add(new Node(startNode, dist[startNode]));
+        // 1 -> v1 -> v2 -> N
+        int v1v2 = 0;
+        if (one_to_v1 == Integer.MAX_VALUE || v1_to_v2 == Integer.MAX_VALUE
+            || v2_to_N == Integer.MAX_VALUE) {
+            v1v2 = Integer.MAX_VALUE;
+        } else {
+            v1v2 = one_to_v1 + v1_to_v2 + v2_to_N;
+        }
+        // 1 -> v2 -> v1 -> N
+        int v2v1 = 0;
+        if (one_to_v2 == Integer.MAX_VALUE || v2_to_v1 == Integer.MAX_VALUE
+            || v1_to_N == Integer.MAX_VALUE) {
+            v2v1 = Integer.MAX_VALUE;
+        } else {
+            v2v1 = one_to_v2 + v2_to_v1 + v1_to_N;
+        }
 
-		while (!queue.isEmpty()) {
-			Node now = queue.poll();
-			int nowNode = now.idx;
+        int answer = Math.min(v1v2, v2v1) == Integer.MAX_VALUE ? -1 : Math.min(v1v2, v2v1);
+        System.out.println(answer);
+    }
 
-			for (Node next : gragh[nowNode]) {
-				int nextNode = next.idx;
+    public static void dijkstra(int startNode) {
+        Queue<Node> queue = new PriorityQueue<>(((o1, o2) -> o1.cost - o2.cost)); // 비용이 작은 순으로 정렬
+        queue.add(new Node(startNode, 0));
 
-				if (dist[nowNode] + next.cost < dist[nextNode]) {
-					dist[nextNode] = dist[nowNode] + next.cost;
-					queue.add(new Node(nextNode, dist[nextNode]));
-				}
-			}
-		}
-	}
+        while (!queue.isEmpty()) {
+            Node now = queue.poll();
+            int nowNode = now.idx;
 
+            for (Node next : graph[nowNode]) {
+                int nextNode = next.idx;
+                // 'startNode -> nowNode -> nextNode' vs 'startNode -> nextNode' 중에 비용이 더 작은 것을 선택한다.
+                // nowNode를 거쳐가는 비용이 더 작은 경우
+                if (dist[nowNode] + next.cost < dist[nextNode]) {
+                    dist[nextNode] = dist[nowNode] + next.cost; // nextNode를 가는 방법이 업데이트
+                    queue.add(
+                        new Node(nextNode, dist[nextNode])); // nextNode를 거쳐서 다른 정점으로 가는 방법도 업데이트
+                }
+            }
+        }
+    }
 }
