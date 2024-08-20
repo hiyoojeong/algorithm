1122769a01ffda7c3e5ad5604d6a6e97a42f2ce3
조유정 <99192837+hiyoojeong@users.noreply.github.com>
2024-08-20 15:26:43 +0900

    [Silver IV] Title: 설탕 배달, Time: 164 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -4,21 +4,25 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		// 입력
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		sc.close();
 
-		// 출력
-		System.out.println(solution(N));
-	}
+		int fiveCnt = N / 5, threeCnt = -1;
+		while (fiveCnt >= 0) {
+			int five = 5 * fiveCnt; // 5kg 설탕봉지의 총 무게
+			int three = N - five; // 3kg 설탕봉지의 총 무게 = 필요한 설탕 무게 - 5kg 설탕봉지의 총 무게
 
-	public static int solution(int N) {
-		for (int i = N / 5; i >= 0; i--)
-			for (int j = (N - i * 5) / 3; j >= 0; j--)
-				if ((N - i * 5) % 3 == 0)
-					return i + j;
-		return -1;
+			if (three % 3 == 0) { // 3kg 설탕봉지로 해당 무게를 맞출 수 있다면, 선택한다.
+				threeCnt = three / 3;
+				break;
+			}
+
+			fiveCnt--; // 3kg 설탕봉지로 해당 무게를 맞출 수 없다면, 5kg 설탕봉지 수를 줄인다.
+		}
+
+		int result = fiveCnt != -1 ? fiveCnt + threeCnt : -1;
+		System.out.println(result);
 	}
 
 }
