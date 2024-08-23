ed63fc419efa897ce3442c7ecf9ebd1047a0a25f
조유정 <99192837+hiyoojeong@users.noreply.github.com>
2024-08-23 15:21:22 +0900

    [Unrated] Title: [모의 SW 역량테스트] 무선 충전, Time: 207 ms, Memory: 32,872 KB -BaekjoonHub

--- SWEA/Unrated/5644. ［모의 SW 역량테스트］ 무선 충전/［모의 SW 역량테스트］ 무선 충전.java
+++ SWEA/Unrated/5644. ［모의 SW 역량테스트］ 무선 충전/［모의 SW 역량테스트］ 무선 충전.java
@@ -1,172 +1,142 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
-// 무선 충전
+// 5644. 무선 충전
 public class Solution {
 
-    static class Pos {
+	static class Pos {
+		int x, y;
 
-        int x, y;
+		public Pos(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
 
-        public Pos(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
+		public void move(int dir) {
+			switch (dir) {
+			case 1: // 상
+				y--;
+				break;
+			case 2: // 우
+				x++;
+				break;
+			case 3: // 하
+				y++;
+				break;
+			case 4: // 좌
+				x--;
+				break;
+			}
+		}
+	}
 
-        public void move(int dir) {
-            if (dir == 1) { // 상
-                y--;
-            } else if (dir == 2) { // 우
-                x++;
-            } else if (dir == 3) { // 하
-                y++;
-            } else if (dir == 4) { // 좌
-                x--;
-            }
-        }
-    }
+	static class BC {
+		Pos pos;
+		int c, p;
 
-    static class BC {
+		public BC(Pos pos, int c, int p) {
+			this.pos = pos;
+			this.c = c;
+			this.p = p;
+		}
+	}
 
-        Pos pos;
-        int range, power;
+	static int M, A;
+	static int[] moveA, moveB;
+	static BC[] bcs;
 
-        public BC(Pos pos, int range, int power) {
-            this.pos = pos;
-            this.range = range;
-            this.power = power;
-        }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
 
-    }
+		StringBuffer answer = new StringBuffer();
+		for (int test_case = 1; test_case <= T; test_case++) {
+			M = sc.nextInt(); // 총 이동시간
+			A = sc.nextInt(); // BC의 개수
 
-    static int dirCnt; // 총 이동시간
-    static int BCCnt; // BC의 개수
+			moveA = new int[M]; // 사용자 A의 이동 정보
+			moveB = new int[M]; // 사용자 B의 이동 정보
+			bcs = new BC[A]; // BC 정보
 
-    static int[] dirA; // 사용자 A의 이동 정보
-    static int[] dirB; // 사용자 B의 이동 정보
-    static BC[] bcs; // BC의 정보
+			for (int i = 0; i < M; i++) {
+				moveA[i] = sc.nextInt();
+			}
+			for (int i = 0; i < M; i++) {
+				moveB[i] = sc.nextInt();
+			}
+			for (int j = 0; j < A; j++) {
+				bcs[j] = new BC(new Pos(sc.nextInt(), sc.nextInt()), sc.nextInt(), sc.nextInt());
+			}
 
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        StringBuffer answer = new StringBuffer();
+			// 초기 위치 설정
+			Pos posA = new Pos(1, 1);
+			Pos posB = new Pos(10, 10);
 
-        int T = Integer.parseInt(br.readLine());
-        for (int t = 1; t <= T; t++) {
-            st = new StringTokenizer(br.readLine());
-            dirCnt = Integer.parseInt(st.nextToken()); // 총 이동 시간
-            BCCnt = Integer.parseInt(st.nextToken()); // BC의 개수
+			// 초기 위치에서 충전한다.
+			int maxCharge = charge(posA, posB);
 
-            dirA = new int[dirCnt]; // 사용자 A의 이동 정보
-            dirB = new int[dirCnt]; // 사용자 B의 이동 정보
-            bcs = new BC[BCCnt]; // BC의 정보
+			// 이동하며 충전한다.
+			for (int i = 0; i < M; i++) {
+				posA.move(moveA[i]);
+				posB.move(moveB[i]);
+				maxCharge += charge(posA, posB);
+			}
 
-            // 사용자 A의 이동 정보
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < dirCnt; i++) {
-                dirA[i] = Integer.parseInt(st.nextToken());
-            }
-            // 사용자 B의 이동 정보
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < dirCnt; i++) {
-                dirB[i] = Integer.parseInt(st.nextToken());
-            }
+			answer.append(String.format("#%d %d\n", test_case, maxCharge));
+		}
 
-            // BC의 정보
-            for (int i = 0; i < BCCnt; i++) {
-                st = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(st.nextToken()); // x 좌표
-                int y = Integer.parseInt(st.nextToken()); // y 좌표
-                int range = Integer.parseInt(st.nextToken()); // 충전 범위
-                int power = Integer.parseInt(st.nextToken()); // 처리량
-                bcs[i] = new BC(new Pos(x, y), range, power);
-            }
+		System.out.println(answer);
 
-            answer.append(String.format("#%d %d\n", t, solution()));
-        }
+		sc.close();
+	}
 
-        System.out.println(answer);
-    }
+	public static int charge(Pos posA, Pos posB) {
+		// 사용자 별로 충전 가능한 BC를 찾는다.
+		List<Integer> chargeA = new ArrayList<>();
+		List<Integer> chargeB = new ArrayList<>();
 
-    public static int solution() {
-        // 초기 좌표
-        Pos userA = new Pos(1, 1);
-        Pos userB = new Pos(10, 10);
+		for (int i = 0; i < A; i++) {
+			if (Math.abs(posA.x - bcs[i].pos.x) + Math.abs(posA.y - bcs[i].pos.y) <= bcs[i].c) {
+				chargeA.add(i);
+			}
+			if (Math.abs(posB.x - bcs[i].pos.x) + Math.abs(posB.y - bcs[i].pos.y) <= bcs[i].c) {
+				chargeB.add(i);
+			}
+		}
 
-        // 최대 충전량
-        int result = 0;
+		int maxCharge = 0;
 
-        // 초기 좌표에서 충전
-        result += charge(userA, userB);
+		// 사용자 A의 BC만 존재
+		if (chargeA.size() > 0 && chargeB.size() == 0) {
+			for (int i = 0; i < chargeA.size(); i++) {
+				maxCharge = Math.max(maxCharge, bcs[chargeA.get(i)].p);
+			}
+		}
+		// 사용자 B의 BC만 존재
+		else if (chargeA.size() == 0 && chargeB.size() > 0) {
+			for (int i = 0; i < chargeB.size(); i++) {
+				maxCharge = Math.max(maxCharge, bcs[chargeB.get(i)].p);
+			}
+		}
+		// 충전할 수 있는 BC들의 조합으로, 현재 위치에서 최대 충전량을 구한다.
+		else {
+			for (int i = 0; i < chargeA.size(); i++) {
+				for (int j = 0; j < chargeB.size(); j++) {
+					// 한 BC에 두 명의 사용자가 접속한 경우, 접속한 사용자의 수만큼 충전 양을 균등하게 분배한다.
+					if (chargeA.get(i) == chargeB.get(j)) {
+						maxCharge = Math.max(maxCharge, bcs[chargeA.get(i)].p);
+						continue;
+					}
 
-        // 이동 후 좌표에서 충전
-        for (int i = 0; i < dirCnt; i++) {
-            userA.move(dirA[i]);
-            userB.move(dirB[i]);
-            result += charge(userA, userB);
-        }
+					// 두 명의 사용자가 다른 BC에서 충전하는 경우, 각각 충전량을 저해준다.
+					maxCharge = Math.max(maxCharge, bcs[chargeA.get(i)].p + bcs[chargeB.get(j)].p);
+				}
+			}
+		}
 
-        return result;
-    }
-
-    public static int charge(Pos userA, Pos userB) {
-        List<Integer> chargeA = new ArrayList<>(); // A를 충전할 수 있는 BC 정보
-        List<Integer> chargeB = new ArrayList<>(); // B를 충전할 수 있는 BC 정보
-
-        for (int i = 0; i < BCCnt; i++) {
-            int distA = Math.abs(userA.x - bcs[i].pos.x) + Math.abs(
-                userA.y - bcs[i].pos.y); // 사용자 A와 BC와의 거리
-            int distB = Math.abs(userB.x - bcs[i].pos.x) + Math.abs(
-                userB.y - bcs[i].pos.y); // 사용자 B와 BC와의 거리
-
-            if (bcs[i].range >= distA) { // BC의 충전 범위내이므로, A를 충전할 수 있다.
-                chargeA.add(i);
-            }
-            if (bcs[i].range >= distB) { // BC의 충전 범위내이므로, B를 충전할 수 있다.
-                chargeB.add(i);
-            }
-        }
-
-        int max = 0;
-
-        // A와 B 둘다 충전할 수 있는 BC가 존재하는 경우
-        if (chargeA.size() > 0 && chargeB.size() > 0) {
-            for (int i : chargeA) {
-                for (int j : chargeB) {
-                    int temp = 0;
-                    // 같은 BC인 경우, 처치량을 나눠가지므로 하나의 처리량만 더해준다.
-                    if (i == j) {
-                        temp += bcs[i].power;
-                    }
-                    // 다른 BC의 경우, 각각 처리량을 더해준다.
-                    else {
-                        temp += bcs[i].power;
-                        temp += bcs[j].power;
-                    }
-
-                    max = Math.max(temp, max);
-                }
-            }
-        }
-        // A만 충전할 수 있는 BC가 존재하는 경우
-        else if (chargeA.size() > 0 && chargeB.size() == 0) {
-            for (int i : chargeA) {
-                max = Math.max(bcs[i].power, max);
-            }
-        }
-        // B만 충전할 수 있는 BC가 존재하는 경우
-        else if (chargeA.size() == 0 && chargeB.size() > 0) {
-            for (int i : chargeB) {
-                max = Math.max(bcs[i].power, max);
-            }
-        }
-
-        return max;
-    }
-
+		return maxCharge;
+	}
 
 }
