6752593aacdbb66e98665f3337e2a7a506f54993
조유정 <99192837+hiyoojeong@users.noreply.github.com>
2024-08-28 16:41:12 +0900

    [Silver II] Title: 촌수계산, Time: 100 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -1,78 +1,66 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayDeque;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
-// 촌수계산
-public class Main {
+// 촌수 계산
+class Main {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
-		int N = Integer.parseInt(br.readLine());
+		int N = Integer.parseInt(br.readLine()); // 사람 수
 
 		st = new StringTokenizer(br.readLine());
-		int n1 = Integer.parseInt(st.nextToken());
-		int n2 = Integer.parseInt(st.nextToken());
+		int a = Integer.parseInt(st.nextToken()); // 촌수 계산해야 하는 사람 1
+		int b = Integer.parseInt(st.nextToken()); // 촌수 계산해야 하는 사람 2
 
-		// 자식들의 부모 정보를 저장한다.
+		boolean[][] map = new boolean[N + 1][N + 1]; // 부모자식 관계
 		int M = Integer.parseInt(br.readLine());
-		Map<Integer, Integer> parents = new HashMap<>();
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
-			int parent = Integer.parseInt(st.nextToken());
-			int child = Integer.parseInt(st.nextToken());
-
-			parents.put(child, parent);
+			int p = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			map[p][c] = true;
+			map[c][p] = true;
 		}
 
-		// n1에서 최고 조상까지 올라가며, 촌수를 저장한다.
-		Map<Integer, Integer> n1_degrees = new HashMap<>();
-		int parent = n1;
-		int n1_degree = 0;
-		while (true) {
-			// 촌수 저장!!
-			n1_degrees.put(parent, n1_degree);
+		// bfs
+		boolean[] visited = new boolean[N + 1];
+		visited[a] = true;
 
-			int child = parent;
+		Queue<Integer> queue = new ArrayDeque<>();
+		queue.add(a);
 
-			// 현재 child가 최고 조상이다.
-			if (!parents.containsKey(child)) {
-				break;
+		int res = -1, depth = 0;
+		while (!queue.isEmpty()) {
+
+			int size = queue.size(); // 현재 큐에 들어있는 정점은 모두 동일한 depth
+			for (int i = 0; i < size; i++) { // 다음 depth에 있는 정점 탐색
+				int now = queue.poll();
+
+				if (now == b) {
+					res = depth;
+					break;
+				}
+
+				for (int j = 1; j <= N; j++) {
+					if (visited[j] || !map[now][j]) { // 방문했거나, 부모자식 관계가 아닌 경우
+						continue;
+					}
+					visited[j] = true;
+					queue.add(j);
+				}
 			}
 
-			parent = parents.get(child);
-			n1_degree++;
+			depth++;
 		}
 
-		// n2에서 최고 조상까지 올라가며, n1과의 공통 조상을 확인한다.
-		int answer = -1;
-		parent = n2;
-		int n2_degree = 0;
-		while (true) {
-			// 공통 조상 발견!!
-			if (n1_degrees.containsKey(parent)) {
-				n1_degree = n1_degrees.get(parent);
-				answer = n1_degree + n2_degree;
-				break;
-			}
-			
-			int child = parent;
-			
-			// 현재 child가 최고 조상이다.
-			if (!parents.containsKey(child)) {
-				break;
-			}
-
-			parent = parents.get(child);
-			n2_degree++;
-
-		}
-
-		System.out.println(answer);
+		System.out.println(res);
 
 	}
 
