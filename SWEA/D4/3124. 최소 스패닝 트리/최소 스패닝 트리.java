b15271a90791846cf5e5dbf4e56d4b6e3cc3ee27
조유정 <99192837+hiyoojeong@users.noreply.github.com>
2024-08-30 13:29:55 +0900

    [D4] Title: 최소 스패닝 트리, Time: 7,573 ms, Memory: 297,308 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Scanner;
@@ -5,23 +8,15 @@ import java.util.Scanner;
 // 3124. 최소 스패닝 트리
 public class Solution {
 
-	static class Edge implements Comparable<Edge> {
-		int from, to, weight;
+	static class Edge {
+		int to, weight;
 
-		public Edge(int from, int to, int weight) {
-			this.from = from;
+		public Edge(int to, int weight) {
 			this.to = to;
 			this.weight = weight;
 		}
 
-		@Override
-		public int compareTo(Edge o) {
-			return this.weight - o.weight;
-		}
-
 	}
-
-	static int[] parents;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -33,55 +28,52 @@ public class Solution {
 			int V = sc.nextInt();
 			int E = sc.nextInt();
 
-			parents = new int[V + 1];
+			List<Edge>[] edges = new ArrayList[V + 1];
 			for (int i = 1; i <= V; i++) {
-				parents[i] = i;
+				edges[i] = new ArrayList<>();
 			}
 
-			Queue<Edge> pq = new PriorityQueue<>();
 			for (int i = 0; i < E; i++) {
 				int from = sc.nextInt();
 				int to = sc.nextInt();
 				int weight = sc.nextInt();
-				pq.add(new Edge(from, to, weight));
+				edges[from].add(new Edge(to, weight));
+				edges[to].add(new Edge(from, weight));
 			}
 
-			int edge = 0;
-			long weight = 0;
+			// prim
+			Queue<Edge> pq = new PriorityQueue<>((o1, o2) -> o1.weight - o2.weight);
+			boolean[] visited = new boolean[V + 1];
+
+			visited[1] = true;
+			for (Edge e : edges[1]) {
+				pq.add(e);
+			}
+
+			long minWeight = 0;
 			while (!pq.isEmpty()) {
-				Edge e = pq.poll();
-				if (union(e.from, e.to)) {
-					weight += e.weight;
-					edge++;
+				Edge now = pq.poll();
+
+				if (visited[now.to]) {
+					continue;
 				}
-				if (edge == V - 1) {
-					break;
+
+				visited[now.to] = true;
+				minWeight += now.weight;
+
+				for (Edge e : edges[now.to]) {
+					if (visited[e.to]) {
+						continue;
+					}
+					pq.add(e);
 				}
 			}
 
-			answer.append("#" + test_case + " " + weight + "\n");
+			answer.append("#" + test_case + " " + minWeight + "\n");
 		}
 
 		System.out.println(answer);
 		sc.close();
 	}
 
-	public static int find(int v) {
-		if (parents[v] == v) {
-			return v;
-		}
-		return parents[v] = find(parents[v]);
-	}
-
-	public static boolean union(int a, int b) {
-		int pa = find(a);
-		int pb = find(b);
-
-		if (pa == pb) {
-			return false;
-		}
-
-		parents[pa] = pb;
-		return true;
-	}
 }
