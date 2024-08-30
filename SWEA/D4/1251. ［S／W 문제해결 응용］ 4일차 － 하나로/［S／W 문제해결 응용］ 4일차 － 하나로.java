0ba5cd6d5b565e6b8e8d22b755ee099ca7515687
조유정 <99192837+hiyoojeong@users.noreply.github.com>
2024-08-30 14:45:17 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 1,589 ms, Memory: 123,332 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -1,33 +1,23 @@
 
-// 하나로
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
 import java.util.Scanner;
 
+// 하나로
 public class Solution {
 
-	static class Edge implements Comparable<Edge> {
-		int from, to;
-		double length;
+	static class Edge {
+		int to;
+		double weight;
 
-		public Edge(int from, int to, double length) {
-			this.from = from;
+		public Edge(int to, double weight) {
 			this.to = to;
-			this.length = length;
-		}
-
-		@Override
-		public int compareTo(Edge o) {
-			if (this.length > o.length) {
-				return 1;
-			} else if (this.length < o.length) {
-				return -1;
-			}
-			return 0;
+			this.weight = weight;
 		}
 	}
-
-	static int[] parents;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -38,72 +28,78 @@ public class Solution {
 			// 입력
 			int N = sc.nextInt(); // 섬의 개수
 
-			int[] x = new int[N + 1]; // 섬의 좌표
-			int[] y = new int[N + 1]; // 섬의 좌표
-			for (int i = 1; i <= N; i++) {
+			int[] x = new int[N]; // 섬의 x좌표
+			int[] y = new int[N]; // 섬의 y좌표
+			for (int i = 0; i < N; i++) {
 				x[i] = sc.nextInt();
 			}
-			for (int i = 1; i <= N; i++) {
+			for (int i = 0; i < N; i++) {
 				y[i] = sc.nextInt();
 			}
 
 			double E = sc.nextDouble(); // 환경 부담 세율
 
-			// 간선 구하기
-			Queue<Edge> pq = new PriorityQueue<>();
-			for (int i = 1; i <= N - 1; i++) {
-				for (int j = i + 1; j <= N; j++) {
-					int from = i;
-					int to = j;
-					double length = Math.sqrt(Math.pow(x[i] - x[j], 2) + Math.pow(y[i] - y[j], 2));
-					pq.add(new Edge(from, to, length));
+			// 간선 추가
+			List<Edge>[] edges = new ArrayList[N];
+			for (int i = 0; i < N; i++) {
+				edges[i] = new ArrayList<>();
+			}
+			for (int i = 0; i < N - 1; i++) {
+				for (int j = i + 1; j < N; j++) {
+//					double L = Math.sqrt(Math.pow(x[i] - x[j], 2) + Math.pow(y[i] - y[j], 2));
+//					L = E * L * L;
+					double L = Math.pow(x[i] - x[j], 2) + Math.pow(y[i] - y[j], 2);
+					edges[i].add(new Edge(j, L));
+					edges[j].add(new Edge(i, L));
 				}
 			}
 
-			// 집합 초기화
-			parents = new int[N + 1];
-			for (int i = 1; i <= N; i++) {
-				parents[i] = i;
+			// prim
+			Queue<Edge> pq = new PriorityQueue<>(new Comparator<Edge>() {
+
+				@Override
+				public int compare(Edge o1, Edge o2) {
+					if (o1.weight > o2.weight) {
+						return 1;
+					} else if (o1.weight < o2.weight) {
+						return -1;
+					}
+					return 0;
+				}
+			});
+			boolean[] visited = new boolean[N];
+
+			visited[0] = true;
+			for (Edge e : edges[0]) {
+				pq.add(e);
 			}
 
-			// Kruskal
-			int edge = 0;
-			double cost = 0;
+			double minWeight = 0;
 			while (!pq.isEmpty()) {
-				Edge e = pq.poll();
-				if (union(e.from, e.to)) {
-					edge++;
-					cost += (e.length * e.length * E);
+				Edge now = pq.poll();
+
+				if (visited[now.to]) {
+					continue;
 				}
-				if (edge == N - 1) {
-					break;
+
+				visited[now.to] = true;
+				minWeight += now.weight;
+
+				for (Edge e : edges[now.to]) {
+					if (visited[e.to]) {
+						continue;
+					}
+					pq.add(e);
 				}
+
 			}
 
-			answer.append("#" + test_case + " " + Math.round((cost * 10) / 10) + "\n");
+			long result = Math.round(minWeight * E);
+//			answer.append("#" + test_case + " " + (long) Math.floor(minWeight * 10) / 10 + "\n");
+			
+			System.out.printf("#%d %d\n", test_case, result);
 		}
 
-		System.out.println(answer);
 		sc.close();
 	}
-
-	public static int find(int v) {
-		if (parents[v] == v) {
-			return v;
-		}
-		return parents[v] = find(parents[v]);
-	}
-
-	public static boolean union(int a, int b) {
-		int pa = find(a);
-		int pb = find(b);
-
-		if (pa == pb) {
-			return false;
-		}
-
-		parents[pa] = pb;
-		return true;
-	}
-
 }
