bd7414d5c0da01b66aa7d29a28524a409041b3bf
조유정 <99192837+hiyoojeong@users.noreply.github.com>
2024-09-03 15:29:34 +0900

    [Gold IV] Title: 최단경로, Time: 672 ms, Memory: 111260 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,7 +1,9 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
@@ -11,69 +13,78 @@ import java.util.StringTokenizer;
 public class Main {
 
 	static class Node {
-		int idx, cost;
+		int v, w;
 
-		public Node(int idx, int cost) {
-			this.idx = idx;
-			this.cost = cost;
+		public Node(int v, int w) {
+			this.v = v;
+			this.w = w;
 		}
 	}
 
-	static List<Node>[] graph; // 그래프를 '인접 리스트'로 구현
-	static int[] dist; // 최단거리
+	static int V, E;
+	static List<Node>[] adjList;
+	static int[] minDistance;
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
+		StringBuffer answer = new StringBuffer();
 
 		st = new StringTokenizer(br.readLine());
-		int V = Integer.parseInt(st.nextToken());
-		int E = Integer.parseInt(st.nextToken());
-		int startNode = Integer.parseInt(br.readLine());
+		V = Integer.parseInt(st.nextToken()); // 정점 수
+		E = Integer.parseInt(st.nextToken()); // 간선 수
 
-		// 인접 리스트와 최단거리 정보를 초기화한다.
-		graph = new ArrayList[V + 1];
-		dist = new int[V + 1];
-		for (int i = 1; i < V + 1; i++) {
-			graph[i] = new ArrayList<>();
-			dist[i] = Integer.MAX_VALUE;
+		int K = Integer.parseInt(br.readLine()); // 시작 정점
+
+		// 인접리스트 초기화
+		adjList = new ArrayList[V + 1];
+		for (int i = 1; i <= V; i++) {
+			adjList[i] = new ArrayList<>();
 		}
 
-		// 간선 정보를 인접 리스트에 저장한다.
 		for (int i = 0; i < E; i++) {
 			st = new StringTokenizer(br.readLine());
-			int u = Integer.parseInt(st.nextToken()); // 출발 노드
-			int v = Integer.parseInt(st.nextToken()); // 도착 노드
-			int w = Integer.parseInt(st.nextToken()); // 거리
-			graph[u].add(new Node(v, w));
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+			adjList[u].add(new Node(v, w));
 		}
 
-		dist[startNode] = 0;
-		dijkstra(startNode);
+		// 최소비용 초기화
+		minDistance = new int[V + 1];
+		Arrays.fill(minDistance, Integer.MAX_VALUE);
 
-		for (int i = 1; i < V + 1; i++) {
-			System.out.println(dist[i] != Integer.MAX_VALUE ? dist[i] : "INF");
+		// 시작정점에서 다른정점까지 최소비용 구하기
+		minDistance[K] = 0;
+		dijkstra(K);
+
+		for (int i = 1; i <= V; i++) {
+			answer.append((minDistance[i] == Integer.MAX_VALUE ? "INF" : minDistance[i]) + "\n");
 		}
+		System.out.println(answer);
 	}
 
-	public static void dijkstra(int startNode) {
-		Queue<Node> queue = new PriorityQueue<Node>((o1, o2) -> o1.cost - o2.cost);
-		queue.add(new Node(startNode, dist[startNode]));
+	public static void dijkstra(int start) {
+		Queue<Node> pq = new PriorityQueue<>((o1, o2) -> o1.w - o2.w);
+		pq.add(new Node(start, 0));
 
-		while (!queue.isEmpty()) {
-			Node now = queue.poll();
-			int nowNode = now.idx;
+		boolean[] visited = new boolean[V + 1];
 
-			// 현재 정점과 인접한 정점을 확인한다.
-			for (Node next : graph[now.idx]) {
-				int nextNode = next.idx;
+		while (!pq.isEmpty()) {
+			Node now = pq.poll();
 
-				// dist[nowNode] + next.cost: 현재 정점을 거쳐서 다음 정점으로 가는 비용
-				// dist[nextNode]: 기존에 알고 있던 다음 정점으로 가는 비용
-				// -> '현재 정점을 거쳐서 다음 정점으로 가는 비용'이 '기존에 알고있던 다음 정점으로 가는 비용'보다 저렴하므로, dist[nextNode]를 갱신한다.
-				if (dist[nowNode] + next.cost < dist[nextNode]) {
-					dist[nextNode] = dist[nowNode] + next.cost;
-					queue.add(new Node(nextNode, dist[nextNode]));
+			if (visited[now.v]) {
+				continue;
+			}
+			visited[now.v] = true;
+
+			for (Node next : adjList[now.v]) {
+				if (visited[next.v]) {
+					continue;
+				}
+				if (minDistance[next.v] > minDistance[now.v] + next.w) {
+					minDistance[next.v] = minDistance[now.v] + next.w;
+					pq.add(new Node(next.v, minDistance[next.v]));
 				}
 			}
 		}
