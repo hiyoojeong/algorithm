bf8f4498ae7168b0cec00ed53fa89f04459b97a5
조유정 <99192837+hiyoojeong@users.noreply.github.com>
2024-09-04 16:15:06 +0900

    [Silver III] Title: 2×n 타일링, Time: 100 ms, Memory: 14328 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 2×n 타일링
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		if (N == 1) {
+			System.out.println(1);
+			return;
+		}
+
+		int[] dp = new int[N + 1];
+		dp[1] = 1;
+		dp[2] = 2;
+		for (int i = 3; i <= N; i++) {
+			// i-1번째에 한칸짜리 붙이기 + i-2번째에 두칸짜리 붙이기
+			dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+		}
+
+		System.out.println(dp[N]);
+	}
+
+}
