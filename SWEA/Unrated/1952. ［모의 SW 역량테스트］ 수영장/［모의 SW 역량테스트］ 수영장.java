f387c51382a47304ce5b12913dd4b90d39dc94fa
조유정 <99192837+hiyoojeong@users.noreply.github.com>
2024-09-04 17:03:29 +0900

    [Unrated] Title: [모의 SW 역량테스트] 수영장, Time: 233 ms, Memory: 23,852 KB -BaekjoonHub

--- SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
+++ SWEA/Unrated/1952. ［모의 SW 역량테스트］ 수영장/［모의 SW 역량테스트］ 수영장.java
@@ -1,63 +1,59 @@
+
 import java.util.Scanner;
 
 // 수영장
 public class Solution {
 
-	static int prices[], plans[];
+	static int day, month, month3, year;
+	static int[] plan = new int[12];
 	static int minCost;
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-
 		StringBuffer answer = new StringBuffer();
+
+		int T = sc.nextInt();
 		for (int test_case = 1; test_case <= T; test_case++) {
-			prices = new int[4];
-			plans = new int[12];
+			day = sc.nextInt();
+			month = sc.nextInt();
+			month3 = sc.nextInt();
+			year = sc.nextInt();
 
-			for (int i = 0; i < 4; i++) {
-				prices[i] = sc.nextInt();
-			}
 			for (int i = 0; i < 12; i++) {
-				plans[i] = sc.nextInt();
+				plan[i] = sc.nextInt();
 			}
 
-//			minCost = Integer.MAX_VALUE;
-			minCost = prices[3]; // 1년 이용권
+			minCost = year;
 			dfs(0, 0);
 
 			answer.append(String.format("#%d %d\n", test_case, minCost));
 		}
 
 		System.out.println(answer);
-
 		sc.close();
-
 	}
 
-	public static void dfs(int month, int cost) {
-		if (cost > minCost) {
+	public static void dfs(int cnt, int cost) {
+		if (cost >= minCost) { // 이미 최소 비용을 넘긴 경우
 			return;
 		}
 
-		if (month == 12) {
+		if (cnt >= 12) { // 1월부터 12월까지의 이용 비용이 최소 비용인 경우
 			minCost = cost;
 			return;
 		}
 
-		if (plans[month] == 0) { // 이용계획이 0일이면 그대로 넘긴다.
-			dfs(month + 1, cost);
-			return;
-		}
-
 		// 1일 이용권
-		dfs(month + 1, cost + prices[0] * plans[month]);
+		dfs(cnt + 1, cost + day * plan[cnt]);
+
 		// 1달 이용권
-		dfs(month + 1, cost + prices[1]);
+		dfs(cnt + 1, cost + month);
+
 		// 3달 이용권
-		dfs(Math.min(month + 3, 12), cost + prices[2]);
-		// 1년 이용권
-		dfs(Math.min(month + 12, 12), cost + prices[3]);
+		dfs(cnt + 3, cost + month3);
+
+		// 12달 이용권
+		dfs(cnt + 12, cost + year);
 	}
 
 }
