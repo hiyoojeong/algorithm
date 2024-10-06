ba902c2fa5f90822edac2dbe85f141cc0b4ad003
조유정 <99192837+hiyoojeong@users.noreply.github.com>
2024-10-06 22:10:15 +0900

    [Gold V] Title: 평범한 배낭, Time: 136 ms, Memory: 14620 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -4,40 +4,32 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 // 평범한 배낭
-public class Main {
+class Main {
 
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder answer = new StringBuilder();
 
-		st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken()); // 물건의 개수
-		int K = Integer.parseInt(st.nextToken()); // 최대 무게
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()); // 물품의 수
+        int K = Integer.parseInt(st.nextToken()); // 버틸 수 있는 무게
 
-		int[] weight = new int[N + 1];
-		int[] value = new int[N + 1];
-		int[] dp = new int[K + 1]; // 최대 무게 K인 가방에 담을 수 있는 최대 가치
-		for (int i = 1; i <= N; i++) {
-			st = new StringTokenizer(br.readLine());
-			int w = Integer.parseInt(st.nextToken());
-			int v = Integer.parseInt(st.nextToken());
+        int[] w = new int[N]; // 물건의 무게
+        int[] v = new int[N]; // 물건의 가치
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            w[i] = Integer.parseInt(st.nextToken());
+            v[i] = Integer.parseInt(st.nextToken());
+        }
 
-			weight[i] = w;
-			value[i] = v;
-		}
-
-		for (int i = 1; i <= N; i++) {
-
-			// 담을 물건 수를 늘려가면서,
-			// 최대 무게(K)부터 담을 수 있는 무게까지만 탐색한다.
-			for (int j = K; j - weight[i] >= 0; j--) {
-				dp[j] = Math.max(dp[j], value[i] + dp[j - weight[i]]);
-			}
-
-		}
-
-		System.out.println(dp[K]);
-
-	}
-
+        int[] dp = new int[K + 1];
+        for (int i = 0; i < N; i++) {
+            for (int j = K; j - w[i] >= 0; j--) {
+                // i번째 물건을 담는게 더 이득인 경우
+                dp[j] = Math.max(dp[j], v[i] + dp[j - w[i]]);
+            }
+        }
+        System.out.println(dp[K]);
+    }
 }
