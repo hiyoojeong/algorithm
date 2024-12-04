29c5ba7763c8614e910dcce2c45f3b60dbc86290
조유정 <99192837+hiyoojeong@users.noreply.github.com>
2024-12-05 00:19:21 +0900

    [Gold IV] Title: 전력난, Time: 792 ms, Memory: 248496 KB -BaekjoonHub

--- 백준/Gold/6497. 전력난/전력난.java
+++ 백준/Gold/6497. 전력난/전력난.java
@@ -1,118 +1,90 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 // 전력난
 public class Main {
 
-	static class Node implements Comparable<Node> {
-		int from, to, cost;
+    static class Edge {
 
-		public Node(int from, int to, int cost) {
-			this.from = from;
-			this.to = to;
-			this.cost = cost;
-		}
+        int v, u, cost;
 
-		@Override
-		public int compareTo(Node o) {
-			// 거리가 작은 순
-			return this.cost - o.cost;
-		}
-	}
+        public Edge(int v, int u, int cost) {
+            this.v = v;
+            this.u = u;
+            this.cost = cost;
+        }
+    }
 
-	static int v;
-	static int e;
-	static List<Node> list;
-	static int[] parents;
+    static int[] parent;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder answer = new StringBuilder();
-		StringTokenizer st;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder answer = new StringBuilder();
 
-		while (true) {
-			st = new StringTokenizer(br.readLine());
-			v = Integer.parseInt(st.nextToken());
-			e = Integer.parseInt(st.nextToken());
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+            int m = Integer.parseInt(st.nextToken());
+            int n = Integer.parseInt(st.nextToken());
 
-			if (v == 0 && e == 0) {
-				break;
-			}
+            if(m == 0 && n == 0) {
+                break;
+            }
 
-			list = new ArrayList<>();
-			parents = new int[v];
+            parent = new int[m];
+            for (int i = 0; i < m; i++) {
+                parent[i] = i;
+            }
 
-			int allCost = 0;
-			for (int i = 0; i < e; i++) {
-				st = new StringTokenizer(br.readLine());
-				int from = Integer.parseInt(st.nextToken());
-				int to = Integer.parseInt(st.nextToken());
-				int cost = Integer.parseInt(st.nextToken());
-				allCost += cost;
+            Queue<Edge> edges = new PriorityQueue<>((o1, o2) -> o1.cost - o2.cost);
+            int totalCost = 0;
+            for (int i = 0; i < n; i++) {
+                st = new StringTokenizer(br.readLine());
+                int v = Integer.parseInt(st.nextToken());
+                int u = Integer.parseInt(st.nextToken());
+                int cost = Integer.parseInt(st.nextToken());
+                edges.add(new Edge(v, u, cost));
+                totalCost += cost;
+            }
 
-				list.add(new Node(from, to, cost));
-			}
+            int cost = 0, edgeCnt = 0;
+            while (!edges.isEmpty()) {
+                if (edgeCnt == m - 1) {
+                    break;
+                }
+                Edge e = edges.poll();
+                if (union(e.v, e.u)) {
+                    cost += e.cost;
+                    edgeCnt++;
+                }
+            }
 
-			// 거리가 작은 순으로 정렬
-			Collections.sort(list, new Comparator<Node>() {
-				@Override
-				public int compare(Node o1, Node o2) {
-					return o1.cost - o2.cost;
-				}
-			});
+            answer.append(totalCost - cost).append("\n");
+        }
 
-			for (int i = 0; i < v; i++) {
-				parents[i] = i;
-			}
+        System.out.println(answer);
+    }
 
-			int sum = 0;
-			int cnt = 0;
+    private static int find(int v) {
+        if (parent[v] == v) {
+            return v;
+        }
+        return parent[v] = find(parent[v]);
+    }
 
-			for (Node n : list) {
-				// 사이클이 생기지 않는다면, 선택한다.
-				if (union(n.from, n.to)) {
-					sum += n.cost;
-					cnt++;
+    private static boolean union(int a, int b) {
+        int pa = find(a);
+        int pb = find(b);
 
-					if (cnt == e - 1)
-						break;
-				}
-			}
-			
-			answer.append(allCost - sum).append("\n");
-		}
-		
-		System.out.println(answer);
-	}
+        if (pa == pb) {
+            return false;
+        }
 
-	private static boolean union(int from, int to) {
-
-		// 각 노드와 연결된 시작노드를 찾는다
-		int fromRoot = findSet(from);
-		int toRoot = findSet(to);
-
-		// 사이클이 생기는 경우
-		if (fromRoot == toRoot) {
-			return false;
-		}
-
-		// 사이클이 생기지 않는 경우, 연결
-		parents[toRoot] = fromRoot;
-		return true;
-	}
-
-	private static int findSet(int v) {
-		if (parents[v] == v) {
-			return v;
-		} else {
-			return parents[v] = findSet(parents[v]);
-		}
-	}
-
+        parent[pa] = pb;
+        return true;
+    }
 }
