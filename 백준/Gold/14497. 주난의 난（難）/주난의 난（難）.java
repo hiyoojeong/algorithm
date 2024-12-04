5f990d274c76e221eb94cdd6f8b67587ccf135ac
조유정 <99192837+hiyoojeong@users.noreply.github.com>
2024-12-05 00:50:07 +0900

    [Gold IV] Title: 주난의 난(難), Time: 448 ms, Memory: 110844 KB -BaekjoonHub

--- 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
+++ 백준/Gold/14497. 주난의 난（難）/주난의 난（難）.java
@@ -1,97 +1,72 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayDeque;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 // 주난의 난
 public class Main {
 
-	static class Pos {
-		int x, y;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
 
-		public Pos(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder answer = new StringBuilder();
 
-	static int N, M;
-	static char[][] map;
+        st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-	static int sx, sy, ex, ey;
+        st = new StringTokenizer(br.readLine());
+        int x1 = Integer.parseInt(st.nextToken()) - 1; // 주난이의 위치
+        int y1 = Integer.parseInt(st.nextToken()) - 1;
+        int x2 = Integer.parseInt(st.nextToken()) - 1; // 범인의 위치
+        int y2 = Integer.parseInt(st.nextToken()) - 1;
 
-	static int[] dx = { -1, 1, 0, 0 };
-	static int[] dy = { 0, 0, 1, -1 };
+        boolean[][] map = new boolean[N][M];
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < M; j++) {
+                char ch = input.charAt(j);
+                if (ch == '*' || ch == '0') { // 장애물 없음
+                    map[i][j] = false;
+                } else { // 장애물 있음
+                    map[i][j] = true;
+                }
+            }
+        }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
+        int cnt = 0;
+        while (true) {
+            cnt++;
 
-		st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		map = new char[N][M];
-
-		st = new StringTokenizer(br.readLine());
-		sx = Integer.parseInt(st.nextToken()) - 1;
-		sy = Integer.parseInt(st.nextToken()) - 1;
-		ex = Integer.parseInt(st.nextToken()) - 1;
-		ey = Integer.parseInt(st.nextToken()) - 1;
-
-		for (int i = 0; i < N; i++) {
-			String line = br.readLine();
-			for (int j = 0; j < M; j++) {
-				map[i][j] = line.charAt(j);
-			}
-		}
-
-		System.out.println(solution());
-
-	}
-
-	public static int solution() {
-		int cnt = 1;
-
-		while (!dfs()) {
-			cnt++;
-		}
-
-		return cnt;
-	}
-
-	public static boolean dfs() {
-		Queue<Pos> queue = new LinkedList<>();
-		queue.add(new Pos(sx, sy));
-
-		boolean[][] visited = new boolean[N][M];
-		visited[sx][sy] = true;
-
-		while (!queue.isEmpty()) {
-			Pos now = queue.poll();
-
-			if (now.x == ex && now.y == ey) {
-				return true;
-			}
-
-			for (int i = 0; i < 4; i++) {
-				int nextx = now.x + dx[i];
-				int nexty = now.y + dy[i];
-
-				if (nextx >= 0 && nexty >= 0 && nextx < N && nexty < M) {
-					if (map[nextx][nexty] == '1') {
-						map[nextx][nexty] = '0';
-						visited[nextx][nexty] = true;
-					} else if (!visited[nextx][nexty]) {
-						queue.add(new Pos(nextx, nexty));
-						visited[nextx][nexty] = true;
-					}
-				}
-			}
-		}
-
-		return false;
-	}
-
+            Queue<int[]> q = new ArrayDeque<>();
+            boolean[][] visited = new boolean[N][M];
+            q.add(new int[]{x1, y1});
+            visited[x1][y1] = true;
+            while (!q.isEmpty()) {
+                int[] now = q.poll();
+                for (int i = 0; i < 4; i++) {
+                    int nx = now[0] + dx[i];
+                    int ny = now[1] + dy[i];
+                    if (nx < 0 || ny < 0 || nx >= N || ny >= M || visited[nx][ny]) {
+                        continue;
+                    }
+                    if (nx == x2 && ny == y2) { // 범인
+                        System.out.println(cnt);
+                        System.exit(0);
+                    }
+                    visited[nx][ny] = true;
+                    if (map[nx][ny]) { // 장애물이 있으면, 없애고 탐색종료
+                        map[nx][ny] = false;
+                    } else { // 장애물이 없으면, 탐색진행
+                        q.add(new int[]{nx, ny});
+                    }
+                }
+            }
+        }
+    }
 }
