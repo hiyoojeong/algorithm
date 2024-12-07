9083409bfb953af9b4d199b714c028db36e7a7c1
조유정 <99192837+hiyoojeong@users.noreply.github.com>
2024-12-07 21:42:29 +0900

    [Gold II] Title: 가운데를 말해요, Time: 476 ms, Memory: 39696 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -1,8 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 // 가운데를 말해요
 public class Main {
@@ -11,28 +12,27 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(br.readLine());
-        List<Integer> list = new ArrayList<>();
+        Queue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder()); // 중앙값 이하
+        Queue<Integer> minHeap = new PriorityQueue<>(); // 중앙값 초과
 
+        int N = Integer.parseInt(br.readLine());
         for (int i = 0; i < N; i++) {
             int num = Integer.parseInt(br.readLine());
 
-            // 이진탐색으로 하계영역에 값 추가
-            int left = 0;
-            int right = list.size();
-            while (left < right) {
-                int mid = (left + right) / 2;
-                if (list.get(mid) < num) {
-                    left = mid + 1;
-                } else {
-                    right = mid;
-                }
+            if (maxHeap.size() == minHeap.size()) { // 앞부분(중앙값 이하)에 넣기
+                maxHeap.add(num);
+            } else { // 뒷부분(중앙값 초과)에 넣기
+                minHeap.add(num);
             }
-            list.add(right, num);
 
-            // 중앙값 출력
-            sb.append(list.get((list.size() - 1) / 2) + "\n");
+            if (!minHeap.isEmpty() && maxHeap.peek() > minHeap.peek()) { // 순서가 꼬였다면 peek끼리 교환
+                minHeap.add(maxHeap.poll());
+                maxHeap.add(minHeap.poll());
+            }
+
+            sb.append(maxHeap.peek() + "\n");
         }
+
         System.out.println(sb);
     }
 }
